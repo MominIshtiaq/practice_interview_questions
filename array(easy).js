8e1d79a0927d3cf25392de9ed1e00194c3571019
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-01 22:04:48 +0500

    interview practice

---
+++ array(easy).js
@@ -0,0 +1,180 @@
+//const array: number[] = [14, 48, 66, 52, 20, 36];
+function largest_Element(arr) {
+    var max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+function largest_Element_02(arr) {
+    // this also sorts the array.
+    //   let newArr = arr.sort((a, b) => a - b);
+    //   return newArr[newArr.length - 1];
+    return Math.max.apply(Math, arr);
+}
+//console.log(`largest Element in array: ${largest_Element_02(array)}`);
+var second_Largest_Element = function (arr) {
+    var max = arr[0];
+    var secMax = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secMax = max;
+            max = arr[i];
+        }
+        if (arr[i] < max && arr[i] > secMax) {
+            secMax = arr[i];
+        }
+    }
+    return secMax;
+};
+/* console.log(
+   `Second largest Element in array: ${second_Largest_Element(array)}`
+); */
+function sortArray(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+//console.log(`the array is sorted: ${sortArray(array)}`);
+//let array02: number[] = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4];
+var removeDuplicates01 = function (arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (result.indexOf(arr[i]) == -1) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+// console.log(`duplicate removed from the array first way: ${removeDuplicates01(array02)}`);
+var removeDuplicates02 = function (arr) {
+    var set = new Set(arr);
+    arr.length = 0;
+    set.forEach(function (item) {
+        arr.push(item);
+    });
+    return arr;
+};
+// console.log(`duplicate removed from the array second way: ${removeDuplicates02(array02)}`);
+var removeDuplicates03 = function (arr) {
+    var temp = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] === temp) {
+            arr.splice(i, 1);
+            i--;
+        }
+        else {
+            temp = arr[i];
+        }
+    }
+    return arr;
+};
+/* console.log(
+  `duplicate removed from the array third way: ${removeDuplicates03(array02)}`
+); */
+var removeDuplicates04 = function (arr) {
+    var temp = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (temp === arr[i]) {
+            arr[i] = "_";
+        }
+        else {
+            temp = arr[i];
+        }
+    }
+    arr.sort();
+    arr = arr.filter(function (x) { return x !== "_"; });
+    return arr;
+};
+/* console.log(
+  `duplicate removed from the array fourth way: ${removeDuplicates04(array02)}`
+); */
+function removeDuplicates05(arr) {
+    var i = 0;
+    for (var j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}
+// const k = removeDuplicates05(array02);
+// console.log(k);
+// console.log(array02);
+// console.log("The array after removing duplicate elements is:");
+// for (let i = 0; i < k; i++) {
+//   console.log(array02[i]);
+// }
+var rotateLeftByOne01 = function (arr) {
+    if (arr.length === 0 || arr.length === 1) {
+        return arr;
+    }
+    var temp = arr[0];
+    arr.shift();
+    arr.push(temp);
+    return arr;
+};
+var rotateLeftByOne02 = function (arr) {
+    if (arr.length === 0 || arr.length === 1) {
+        return arr;
+    }
+    var i = arr.length - 1;
+    var j = 0;
+    while (i > 0) {
+        var temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+        i--;
+    }
+    return arr;
+};
+var rotateLeftByOne03 = function (arr) {
+    if (arr.length === 0 || arr.length === 1) {
+        return arr;
+    }
+    var temp = arr[0];
+    for (var i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr[arr.length - 1] = temp;
+    return arr;
+};
+// let array03 = [1, 2, 3, 4, 5];
+// console.log(rotateLeftByOne03(array03));
+var rotateElement_K_Times = function (arr, k, direct) {
+    if (direct === "left") {
+        k = Math.floor(k % arr.length);
+        while (k > 0) {
+            var temp = arr[0];
+            for (var i = 0; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length - 1] = temp;
+            k--;
+        }
+        return arr;
+    }
+    else if (direct === "right") {
+        k = Math.floor(k % arr.length);
+        while (k > 0) {
+            var temp = arr[arr.length - 1];
+            for (var i = arr.length - 2; i >= 0; i--) {
+                arr[i + 1] = arr[i];
+            }
+            arr[0] = temp;
+            k--;
+        }
+        return arr;
+    }
+    else {
+        throw new Error("Direction to rotate the array is wrong");
+    }
+};
+var arr = [3, 7, 8, 9, 10, 11];
+console.log(rotateElement_K_Times(arr, 3, "right"));
---
+++ array(easy).ts
@@ -0,0 +1,228 @@
+//const array: number[] = [14, 48, 66, 52, 20, 36];
+
+function largest_Element(arr: number[]): number {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+function largest_Element_02(arr: number[]): number {
+  // this also sorts the array.
+  //   let newArr = arr.sort((a, b) => a - b);
+  //   return newArr[newArr.length - 1];
+
+  return Math.max(...arr);
+}
+
+//console.log(`largest Element in array: ${largest_Element_02(array)}`);
+
+let second_Largest_Element = (arr: number[]): number => {
+  let max: number = arr[0];
+  let secMax: number = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secMax = max;
+      max = arr[i];
+    }
+    if (arr[i] < max && arr[i] > secMax) {
+      secMax = arr[i];
+    }
+  }
+
+  return secMax;
+};
+
+/* console.log(
+   `Second largest Element in array: ${second_Largest_Element(array)}`
+); */
+
+function sortArray(arr: number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//console.log(`the array is sorted: ${sortArray(array)}`);
+
+//let array02: number[] = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4];
+let removeDuplicates01 = (arr: number[]) => {
+  let result: number[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
+
+// console.log(`duplicate removed from the array first way: ${removeDuplicates01(array02)}`);
+
+let removeDuplicates02 = (arr: number[]) => {
+  let set = new Set(arr);
+
+  arr.length = 0;
+
+  set.forEach((item) => {
+    arr.push(item);
+  });
+
+  return arr;
+};
+
+// console.log(`duplicate removed from the array second way: ${removeDuplicates02(array02)}`);
+
+let removeDuplicates03 = (arr: number[]) => {
+  let temp = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === temp) {
+      arr.splice(i, 1);
+      i--;
+    } else {
+      temp = arr[i];
+    }
+  }
+
+  return arr;
+};
+
+/* console.log(
+  `duplicate removed from the array third way: ${removeDuplicates03(array02)}`
+); */
+
+let removeDuplicates04 = (arr: (number | string)[]) => {
+  let temp = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (temp === arr[i]) {
+      arr[i] = "_";
+    } else {
+      temp = arr[i];
+    }
+  }
+
+  arr.sort();
+
+  arr = arr.filter((x) => x !== "_");
+
+  return arr;
+};
+
+/* console.log(
+  `duplicate removed from the array fourth way: ${removeDuplicates04(array02)}`
+); */
+
+function removeDuplicates05(arr: number[]) {
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+// const k = removeDuplicates05(array02);
+// console.log(k);
+// console.log(array02);
+// console.log("The array after removing duplicate elements is:");
+// for (let i = 0; i < k; i++) {
+//   console.log(array02[i]);
+// }
+
+let rotateLeftByOne01 = (arr: number[]) => {
+  if (arr.length === 0 || arr.length === 1) {
+    return arr;
+  }
+
+  let temp: number = arr[0];
+  arr.shift();
+  arr.push(temp);
+
+  return arr;
+};
+
+let rotateLeftByOne02 = (arr: number[]) => {
+  if (arr.length === 0 || arr.length === 1) {
+    return arr;
+  }
+
+  let i = arr.length - 1;
+  let j = 0;
+  while (i > 0) {
+    let temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+    i--;
+  }
+
+  return arr;
+};
+
+let rotateLeftByOne03 = (arr: number[]) => {
+  if (arr.length === 0 || arr.length === 1) {
+    return arr;
+  }
+
+  let temp = arr[0];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr[arr.length - 1] = temp;
+
+  return arr;
+};
+
+// let array03 = [1, 2, 3, 4, 5];
+// console.log(rotateLeftByOne03(array03));
+
+let rotateElement_K_Times = (arr: number[], k: number, direct: string) => {
+  if (direct === "left") {
+    k = Math.floor(k % arr.length);
+
+    while (k > 0) {
+      let temp = arr[0];
+      for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+      }
+      arr[arr.length - 1] = temp;
+      k--;
+    }
+
+    return arr;
+  } else if (direct === "right") {
+    k = Math.floor(k % arr.length);
+
+    while (k > 0) {
+      let temp = arr[arr.length - 1];
+      for (let i = arr.length - 2; i >= 0; i--) {
+        arr[i + 1] = arr[i];
+      }
+      arr[0] = temp;
+      k--;
+    }
+
+    return arr;
+  } else {
+    throw new Error("Direction to rotate the array is wrong");
+  }
+};
+
+let arr = [3, 7, 8, 9, 10, 11];
+console.log(rotateElement_K_Times(arr, 3, "right"));
